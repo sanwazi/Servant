344a74303c11c965ad7cf91ccb961a843e3bd818
leontheprofessional7 <yanglyu.leon.7@gmail.com>
2015-08-26 11:57:27 -0400

    Menu item inflated into main layout

--- app/src/main/java/com/example/tek/first/servant/todolist/ToDoListActivity.java
+++ app/src/main/java/com/example/tek/first/servant/todolist/ToDoListActivity.java
@@ -2,6 +2,8 @@ package com.example.tek.first.servant.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tek.first.servant.R;
 
@@ -11,5 +13,16 @@ public class ToDoListActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_todolist_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
