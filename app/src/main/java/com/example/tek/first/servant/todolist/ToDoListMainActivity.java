6f8191b842502a9e3e8190c035a94426a9bfaf5b
leontheprofessional7 <yanglyu.leon.7@gmail.com>
2015-08-26 13:35:13 -0400

    Dialog box for detailed todolist item improved
    Spinner added.
    Two buttons for DatePicker & TimePicker added.

--- app/src/main/java/com/example/tek/first/servant/todolist/ToDoListMainActivity.java
+++ app/src/main/java/com/example/tek/first/servant/todolist/ToDoListMainActivity.java
@@ -4,15 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.tek.first.servant.R;
 
 public class ToDoListMainActivity extends Activity {
 
+    private ImageButton btnConfirm;
+    private EditText editTextInput;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+        btnConfirm = (ImageButton) findViewById(R.id.todoitemconfirm_btn_activitytodolist);
+        editTextInput = (EditText) findViewById(R.id.todoiteminput_edittext_activitytodolist);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void createToDoListItemDetail(){
+
     }
 
     @Override
